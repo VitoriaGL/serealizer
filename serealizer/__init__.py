ee2393df60e8830bb48c4432cad61bafe1192c3a
Seu Nome <vitoriaglordeiro@gmail.com>
2025-11-24 13:36:40 -0300

    creat: criando as views e os testes unitarios

--- serealizer/__init__.py
+++ serealizer/__init__.py
@@ -3,7 +3,15 @@ Serealizer - Um serializer Python para converter objetos em diferentes formatos.
 """
 
 from .serializer import Serializer, JSONSerializer, DictSerializer
+from .viewsets import JSONSerializerViewSet, DictSerializerViewSet, create_app
 
 __version__ = "0.1.0"
-__all__ = ["Serializer", "JSONSerializer", "DictSerializer"]
+__all__ = [
+    "Serializer",
+    "JSONSerializer",
+    "DictSerializer",
+    "JSONSerializerViewSet",
+    "DictSerializerViewSet",
+    "create_app",
+]
 
